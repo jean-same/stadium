00451a2cfde14d8186fe9880378fbe20e2f1a84c
Jean <jnmauricesam92@gmail.com>
2021-11-01 23:18:25 +0100

    Creation of a Password encoder listener(based on the doctrine orm listener) to hash the accounts password

---
+++ src/EventsListener/Doctrine/PasswordEncoderListener.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\EventsListener\Doctrine;
+
+use App\Entity\Account;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class PasswordEncoderListener
+{
+
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function prePersist(Account $account)
+    {
+        $hash = $this->passwordHasher->hashPassword($account, $account->getPassword());
+        $account->setPassword($hash);
+    }
+
+    public function preUpdate(Account $account)
+    {
+        $hash = $this->passwordHasher->hashPassword($account, $account->getPassword());
+        $account->setPassword($hash);
+    }
+}
