b70fafa4dea154d02ec3977374b43aaba21ea0ae
melaniefauchon <fauchonmelanie@gmail.com>
2021-11-03 15:38:59 +0100

    Implementation of the architecture of member/api and removal templates

--- src/Controller/Api/V1/Member/FilesController.php
+++ src/Controller/Api/V1/Member/FilesController.php
@@ -6,12 +6,45 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/api/v1/member/account/{accountId}/profil/{profilId}/files", name="api_v1_member_account_profil_files")
+ */
 class FilesController extends AbstractController
 {
     /**
-     * @Route("/api/v1/member/files", name="api_v1_member_files")
+     * @Route("/{fileId}", name="read", methods={"GET"})
      */
-    public function index(): Response
+    public function read(): Response
+    {
+        return $this->render('api/v1/member/files/index.html.twig', [
+            'controller_name' => 'FilesController',
+        ]);
+    }
+
+    /**
+     * @Route("/{fileId}", name="edit", methods={"PATCH"})
+     */
+    public function edit(): Response
+    {
+        return $this->render('api/v1/member/files/index.html.twig', [
+            'controller_name' => 'FilesController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="add", methods={"POST"})
+     */
+    public function add(): Response
+    {
+        return $this->render('api/v1/member/files/index.html.twig', [
+            'controller_name' => 'FilesController',
+        ]);
+    }
+
+    /**
+     * @Route("/{fileId}", name="delete", methods={"DELETE"})
+     */
+    public function delete(): Response
     {
         return $this->render('api/v1/member/files/index.html.twig', [
             'controller_name' => 'FilesController',
