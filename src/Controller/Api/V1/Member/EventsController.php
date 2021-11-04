cd78851baa6772b6f8879cbc19723e4dcb08ec0d
Jean <jnmauricesam92@gmail.com>
2021-11-04 20:13:41 +0100

    Register a profil for an event

--- src/Controller/Api/V1/Member/EventsController.php
+++ src/Controller/Api/V1/Member/EventsController.php
@@ -3,24 +3,29 @@
 namespace App\Controller\Api\V1\Member;
 
 use App\Entity\Profil;
+use App\Repository\EventRepository;
 use App\Repository\ProfilRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/api/v1/member/profil/{profilId}/events", name="api_v1_member_account_profil_events")
  */
 class EventsController extends AbstractController
 {
-
-
+    private $entityManager;
+    private $eventsRepository;
     private $profilRepository;
 
-    public function __construct( ProfilRepository $profilRepository )
+    public function __construct( ProfilRepository $profilRepository, EventRepository $eventsRepository , EntityManagerInterface $entityManager)
     {
+        $this->eventsRepository = $eventsRepository;
         $this->profilRepository = $profilRepository;
+        $this->entityManager = $entityManager;
     }
     
     /**
@@ -41,11 +46,18 @@ class EventsController extends AbstractController
     /**
      * @Route("/{eventId}/register", name="register", methods={"POST"})
      */
-    public function register(Request $request): Response
+    public function register( $eventId , $profilId ): Response
     {
-        return $this->render('api/v1/member/events/index.html.twig', [
-            'controller_name' => 'EventsController',
-        ]);
+
+        $event = $this->eventsRepository->find($eventId);
+        $profil = $this->profilRepository->find($profilId);
+
+        $profil->addEvent($event);
+
+        $this->entityManager->persist($profil);
+        $this->entityManager->flush();
+        
+        return $this->json(Response::HTTP_OK);
     }
 
     /**
