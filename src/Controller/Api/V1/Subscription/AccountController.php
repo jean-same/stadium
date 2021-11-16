3589bc5576e89ba16da7ee15088d9e263e13c5cf
Jean <jnmauricesam92@gmail.com>
2021-11-16 11:55:06 +0100

    Implementation of the controller that will handle the creation of new account and the infos for new associations and members

---
+++ src/Controller/Api/V1/Subscription/AccountController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Controller\Api\V1\Subscription;
+
+use App\Entity\Account;
+use App\Repository\AccountRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+/**
+* @Route("/api/v1/subscription/account", name="api_v1_subscription_account")
+*/
+class AccountController extends AbstractController
+{
+    protected $serializer;
+    protected $validator;
+    protected $entityManager;
+    protected $passwordHasher;
+    protected $accountRepository;
+
+    public function __construct(AccountRepository $accountRepository,SerializerInterface $serializer, ValidatorInterface $validator, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher )
+    {
+        $this->serializer = $serializer;
+        $this->validator = $validator;
+        $this->entityManager = $entityManager;
+        $this->passwordHasher = $passwordHasher;
+        $this->accountRepository = $accountRepository;
+    }
+
+    /**
+    * @Route("/", name="", methods={"POST"})
+    */
+    public function addAccount(Request $request): Response
+    {
+        $jsonContent = $request->getContent();
+
+        $account = $this->serializer->deserialize($jsonContent, Account::class, 'json');
+        $errors = $this->validator->validate($account);
+ 
+        $roleAccount = json_decode($jsonContent)->role;
+
+        if($roleAccount ==  "ADHERENT"){
+            $account->setRoles(["ROLE_ADHERENT"]);
+        }
+
+        if($roleAccount ==  "ASSOC"){
+            $account->setRoles(["ROLE_ASSOC"]);
+        }
+
+        if (count($errors) > 0) {
+            $responseAsArray = [
+                'error' => true,
+                'message' => $errors
+            ];
+            return $this->json($responseAsArray, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $this->entityManager->persist($account);
+        $this->entityManager->flush();
+
+
+        $responseAsArray = [
+            'message' => 'Compte crée',
+            'id' => $account->getId(),
+            'email' => $account->getEmail(),
+            'roles' => $account->getRoles(),
+        ];
+        return $this->json($responseAsArray, Response::HTTP_CREATED);
+
+        return $this->render('api/v1/subscription/account/index.html.twig', [
+            'controller_name' => 'AccountController',
+        ]);
+    }
+}
