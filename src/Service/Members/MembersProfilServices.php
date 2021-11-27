2a77b6212bcd29a923df43141aad684a0d62e039
Jean <jnmauricesam92@gmail.com>
2021-11-27 23:28:42 +0100

    Usage of the createNotFoundException method for the member profil service

--- src/Service/Members/MembersProfilServices.php
+++ src/Service/Members/MembersProfilServices.php
@@ -3,26 +3,48 @@
 namespace App\Service\Members;
 
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-class MembersProfilServices{
+class MembersProfilServices
+{
 
     private $security;
 
-    public function __construct( Security $security )
+    public function __construct(Security $security)
     {
         $this->security = $security;
     }
-    public function getProfilFromUser($slug){
+
+    /**
+     * Returns a NotFoundHttpException.
+     *
+     * This will result in a 404 response code. Usage example:
+     *
+     *     throw $this->createNotFoundException('Page not found!');
+     */
+    protected function createNotFoundException(string $message = 'Not Found', \Throwable $previous = null): NotFoundHttpException
+    {
+        return new NotFoundHttpException($message, $previous);
+    }
+
+
+    public function getProfilFromUser($slug)
+    {
         /**@var Account */
         $user = $this->security->getUser();
         $profiles = $user->getProfil();
-        foreach($profiles as $profil){
-            if($profil->getSlug() == $slug ){
+        $profilToShow = null;
+        foreach ($profiles as $profil) {
+            if ($profil->getSlug() == $slug) {
                 $profilToShow = $profil;
             }
         }
 
-        if($profilToShow){
+        if (!$profilToShow) {
+            throw $this->createNotFoundException("Cet adherent n'existe pas");
+        }
+
+        if ($profilToShow) {
             return $profilToShow;
         }
 
