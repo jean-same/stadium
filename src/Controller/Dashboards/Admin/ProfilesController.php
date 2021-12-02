1eda139b576f5172a36f605f9a3093dd26df4cbe
Jean <jnmauricesam92@gmail.com>
2021-12-02 09:56:53 +0100

    Profil delete feature implementation(Admin)

--- src/Controller/Dashboards/Admin/ProfilesController.php
+++ src/Controller/Dashboards/Admin/ProfilesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Dashboards\Admin;
 
+use App\Repository\ProfilRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Service\Admin\AssociationServices;
 use MercurySeries\FlashyBundle\FlashyNotifier;
@@ -14,13 +15,15 @@ class ProfilesController extends AbstractController
 {
     private $em;
     private $flashy;
+    private $profilRepository;
     private $associationServices;
 
 
-    public function __construct(FlashyNotifier $flashy,  EntityManagerInterface $em, AssociationServices $associationServices)
+    public function __construct(FlashyNotifier $flashy,  EntityManagerInterface $em, ProfilRepository $profilRepository , AssociationServices $associationServices)
     {
         $this->em = $em;
         $this->flashy = $flashy;
+        $this->profilRepository = $profilRepository;
         $this->associationServices = $associationServices;
     }
 
@@ -31,5 +34,18 @@ class ProfilesController extends AbstractController
         $profiles = $this->associationServices->getAdherentFromAssoc();
 
         return $this->render('dashboards/admin/profiles/profiles.html.twig', compact('profiles'));
+    }
+
+    #[Route('/delete/{id}', name: 'delete')]
+    public function delete($id){
+        $profil = $this->profilRepository->find($id);
+
+        $this->em->remove($profil);
+
+        $this->em->flush();
+
+        $this->flashy->success('Adhérent supprimé avec succes');
+
+        return $this->redirectToRoute('dashboards_admin_profiles_profiles');
     }
 }
