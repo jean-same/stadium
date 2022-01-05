2e754e20c335ec5874359f6709bc3cf95dfb523a
Jean <jnmauricesam92@gmail.com>
2022-01-05 14:48:34 +0100

    Fix welcome message from superadmin dashboard

--- src/Controller/Dashboards/Superadmin/HomeController.php
+++ src/Controller/Dashboards/Superadmin/HomeController.php
@@ -2,22 +2,24 @@
 
 namespace App\Controller\Dashboards\Superadmin;
 
+use App\Repository\FileRepository;
+use App\Repository\EventRepository;
+use App\Repository\LessonRepository;
 use App\Repository\AccountRepository;
 use App\Repository\ActivityRepository;
 use App\Repository\AssociationRepository;
-use App\Repository\EventRepository;
-use App\Repository\FileRepository;
-use App\Repository\LessonRepository;
 use App\Service\General\ChartGeneratorService;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use MercurySeries\FlashyBundle\FlashyNotifier;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
 * @Route("/dashboards/superadmin", name="dashboards_superadmin_")
 */
 class HomeController extends AbstractController
 {
+    private $flashy;
     private $accountRepository;
     private $activityRepository;
     private $eventRepository;
@@ -25,7 +27,7 @@ class HomeController extends AbstractController
     private $lessonRepository;
     private $chartGeneratorService;
 
-    public function __construct(AssociationRepository $associationRepository, AccountRepository $accountRepository, ActivityRepository $activityRepository, EventRepository $eventRepository, FileRepository $fileRepository, LessonRepository $lessonRepository, ChartGeneratorService $chartGeneratorService)
+    public function __construct(AssociationRepository $associationRepository,FlashyNotifier $flashy , AccountRepository $accountRepository, ActivityRepository $activityRepository, EventRepository $eventRepository, FileRepository $fileRepository, LessonRepository $lessonRepository, ChartGeneratorService $chartGeneratorService)
     {
         $this->accountRepository = $accountRepository;
         $this->associationRepository = $associationRepository;
